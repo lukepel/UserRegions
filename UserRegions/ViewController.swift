9b633f03a5bf204fb92bb094e66913c3d24b5bce
Luke Pelling (i7673375) <i7673375@mac27779.local>
2015-10-26 09:11:22 +0000

    Initial Commit

---
+++ UserRegions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserRegions
+//
+//  Created by Luke Pelling (i7673375) on 26/10/2015.
+//  Copyright © 2015 Luke Pelling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
