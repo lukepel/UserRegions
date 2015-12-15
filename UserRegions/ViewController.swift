bab1e36c1dc03414f44da700174f579eba6594d4
Luke <Luke@Lukes-MacBook-Pro.local>
2015-12-15 10:58:44 +0000

    First Commit

--- UserRegions/ViewController.swift
+++ UserRegions/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myMap: MKMapView!
+    @IBOutlet weak var myImage: UIImageView!
+    
+    let locationManager = CLLocationManager()
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Map tracks the users location whilst zooming in with below code
+        myMap.setUserTrackingMode(.Follow, animated: true)
+   
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        
+        let freshers = CLLocationCoordinate2D(latitude: 50.719799, longitude: -1.879439)
+        let freshersRegion = CLCircularRegion(center: freshers, radius: 100, identifier: "freshers")
+        locationManager.startMonitoringForRegion(freshersRegion)
+        
+        let location = CLLocationCoordinate2D(
+            latitude: 50.716382,
+            longitude: -1.875637
+        )
+        
+        let pierAnnotation = MKPointAnnotation()
+        pierAnnotation.coordinate = location
+        pierAnnotation.title = "Pier Approach"
+        pierAnnotation.subtitle = "Bournemouth"
+        myMap.addAnnotation(pierAnnotation)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    
+    
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("Entering \(region.identifier)")
+        //changes image
+        myImage.image = UIImage(named: region.identifier)
+    
+    }
+    
+    func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("Exiting \(region.identifier)")
+    }
+    
+}
